9671ae97afab6f171a1b01e53dd083e1e2ecaf61
PloKoon68 <ors_mehmet68@hotmail.com>
2024-07-09 18:44:08 +0300

    New Properties

--- src/Processing Page/Cards/Cards.js
+++ src/Processing Page/Cards/Cards.js
@@ -7,21 +7,20 @@ import { useState } from 'react';
 function Cards() {
   const [criteriaCards, setCriteriaCards] = useState([{
     criteriaName: "C1",
-    dataType: "Numerical"
+    dataType: "Categorical",
+    categories: [{categoryName: "Cat1", categoryPoint: 15},
+                 {categoryName: "Cat2", categoryPoint: 10},
+                 {categoryName: "Cat3", categoryPoint: 32}],
+    characteristic: "Beneficial",
+    criteriaPoint: 15
   },
   {
     criteriaName: "C1",
-    dataType: "Numerical"
-  },
-  {
-    criteriaName: "C1",
-    dataType: "Numerical"
-  },
-  {
-    criteriaName: "C1",
-    dataType: "Numerical"
-  }])
-
+    dataType: "Numerical",
+    characteristic: "Beneficial",
+    criteriaPoint: 15
+  }  
+])
   return (
     <div className='criterias'>
       <button type="button" class="btn btn-primary" style={{marginTop:"110px", marginLeft:"10px", marginBottom: "10px"}}>Add Criteria</button>
@@ -29,10 +28,17 @@ function Cards() {
 
         {criteriaCards.map((card) => {
           return <div className="card">
-            <h3>{card.criteriaName}</h3>
-            <p>{card.dataType}</p>
+            <h3 style={{textAlign: "center"}}>{card.criteriaName}</h3>
+            <p><b>Data Type:</b> {card.dataType}</p>
+            {card.categories && <p><b>Data Categories:</b> {card.categories.map((category, index) => {return  (<span key={index}>
+                                                                                                          {category.categoryName}
+                                                                                                          {index !== card.categories.length - 1 ? ", " : ""}
+                                                                                                        </span>)
+                                                                                                      })}</p>}
+            <p><b>Characteristic:</b> {card.characteristic}</p>
+            <p><b>Criteria Point:</b> {card.criteriaPoint}</p>
           </div>
-        })}
+  })}
         
         
         
